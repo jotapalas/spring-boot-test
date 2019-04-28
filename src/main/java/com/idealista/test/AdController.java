ef78cd27aaec5b189d9b9c6996b73438b73bf16c
Juan Palacios <palaciosgalvanj@gmail.com>
2019-04-28 13:29:06 +0200

    Basic score calculation

--- src/main/java/com/idealista/test/AdController.java
+++ src/main/java/com/idealista/test/AdController.java
@@ -10,11 +10,21 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
+
+import java.util.Arrays;
 import java.util.List;
 
 @RestController
 @RequestMapping("/ads")
 public class AdController {
+  public static final String[] IMPORTANT_WORDS = {
+    "luminoso",
+    "nuevo",
+    "céntrico",
+    "reformado",
+    "ático"
+  };
+
   @Autowired
   private AdRepository repository;
   @RequestMapping(value = "", method = RequestMethod.GET)
@@ -28,7 +38,48 @@ public class AdController {
   }
   
   @RequestMapping(value = "", method = RequestMethod.POST)
-  public Ad createAd(@Valid @RequestBody Ad ad) {
+  public List<Ad> createAd(@Valid @RequestBody List<Ad> adsList) {
+    repository.saveAll(adsList);
+    return adsList;
+  }
+
+  @RequestMapping(value = "/{id}/setScore", method = RequestMethod.PUT)
+  public Ad setScore(@PathVariable("id") Integer id) {
+    Ad ad = getAdById(id);
+    Integer score = 0;
+
+    //Que el anuncio no tenga foto resta 10 puntos
+    if (ad.getPictures().size() == 0) {
+      score -= 10;
+    }
+
+    //Cada foto que tenga el anuncio proporciona 20 puntos si es una foto HD o 10 si no lo es
+
+
+    //Que tenga descripción aporta 5 puntos
+    String adDescription = ad.getDescription();
+    if (adDescription != null  && adDescription.length() > 0) {
+      score += 5;
+      String adTypology = ad.getTypology();
+      List<String> adWords = Arrays.asList(adDescription.toLowerCase().split(" "));
+      Integer wordCount = adWords.size();
+      if (adTypology == "FLAT" && wordCount >= 20) {
+        score += wordCount >= 50 ? 30 : 10;
+      }
+      if (adTypology == "CHALET" && wordCount >= 50) {
+        score += 20;
+      }
+      
+      for (String s : IMPORTANT_WORDS) {
+        if (adWords.contains(s)){
+          score += 5;
+        }
+      }
+    }
+    if (ad.isComplete()) {
+      score += 40;
+    }
+    ad.setScore(score);
     repository.save(ad);
     return ad;
   }
--- src/main/java/com/idealista/test/models/Ad.java
+++ src/main/java/com/idealista/test/models/Ad.java
@@ -12,20 +12,25 @@ public class Ad {
   private String description;
   private String typology; //TODO convert to Enum
   private Integer houseSize;
+  private Integer gardenSize;
   private List<Integer> pictures;
+  private Integer score;
 
   public Ad (
     Integer id,
     String description,
     String typology,
     Integer houseSize,
+    Integer gardenSize,
     List<Integer> pictures  
   ) {
     this.id = id;
     this.description = description;
     this.typology = typology;
     this.houseSize = houseSize;
+    this.gardenSize = gardenSize;
     this.pictures = pictures;
+    this.score = 0;
   }
 
   public Integer getId() {
@@ -44,7 +49,41 @@ public class Ad {
     return this.houseSize;
   }
 
+  public Integer getGardenSize() {
+    return this.gardenSize;
+  }
+
   public List<Integer> getPictures() {
     return this.pictures;
+  }
+
+  public Integer getScore() {
+    return this.score;
+  }
+
+  public void setScore(Integer score) {
+    this.score = score;
+  }
+
+  public Boolean isComplete() {
+    Boolean isComplete = 
+      this.pictures != null
+      && this.pictures.size() > 0;
+    
+    if (this.typology != "GARAGE") {
+      isComplete = isComplete 
+        && this.description != null
+        && this.description.length() > 0
+        && this.houseSize != null
+        && this.houseSize > 0;
+      
+      if (this.typology == "CHALET") {
+        isComplete = isComplete
+          && this.gardenSize != null
+          && this.gardenSize > 0;
+      }
+    }
+
+    return isComplete;
   }
 }
